d5a5f9fc4f82e76e1ffe5ba58fbd8ead399ce923
VeuraGJ <yuanxiao_ru@163.com>
2019-07-13 14:45:51 +0800

    feat:implement Story1 AC2

--- src/test/java/com/thoughtworks/tdd/ParkingBoyTest.java
+++ src/test/java/com/thoughtworks/tdd/ParkingBoyTest.java
@@ -15,7 +15,22 @@ public class ParkingBoyTest {
         Ticket ticket = parkingBoy.parkCar(car);
         Car fetchCar = parkingBoy.fetchCar(ticket);
         //then
-        assertSame(fetchCar,car);
+        assertSame(car,fetchCar);
     }
-
+    @Test
+    public void should_fetch_multiple_car_when_give_multiple_ticket(){
+        //given
+        Car firstCar = new Car();
+        Car secondCar = new Car();
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        //when
+        Ticket firstTicket = parkingBoy.parkCar(firstCar);
+        Ticket secondTicket = parkingBoy.parkCar(secondCar);
+        Car fetchFirstCar = parkingBoy.fetchCar(firstTicket);
+        Car fetchSecondCar = parkingBoy.fetchCar(secondTicket);
+        //then
+        assertSame(firstCar,fetchFirstCar);
+        assertSame(secondCar,fetchSecondCar);
+    }
 }
